2b593bcc08df3f61505ac1981bb482df68632dd1
Mr.doob <info@mrdoob.com>
2019-11-17 10:12:02 -0800

    Fixed rendering of non-GLTF files.

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ import { OBJLoader } from './loaders/OBJLoader.js';
 import { PLYLoader } from './loaders/PLYLoader.js';
 import { STLLoader } from './loaders/STLLoader.js';	
 
+import { cubeUVChunk } from './shader-chunk/cube_uv_reflection_fragment.glsl.js';
+import { normalmapChunk } from './shader-chunk/normalmap_pars_fragment.glsl.js';
+
 window.addEventListener( 'load', function () {
 
 	// Experimental: Don't do this at home
 
-	const DEFAULT_MATERAL = new MeshStandardMaterial( { roughness: 1, metalness: 0.5 } );
+	const DEFAULT_MATERAL = new MeshStandardMaterial( { color: 0x606060, roughness: 1.0, metalness: 0.0 } );
+	DEFAULT_MATERAL.onBeforeCompile = function ( shader ) {
+		shader.fragmentShader = shader.fragmentShader
+			.replace( '#include <cube_uv_reflection_fragment>', cubeUVChunk )
+			.replace( '#include <normalmap_pars_fragment>', normalmapChunk );
+	};
 
 	var canvas = viewer.shadowRoot.querySelector( 'canvas' );
 	var poster = viewer.shadowRoot.querySelector( '.slot.poster' );
@@ -41,6 +49,7 @@ window.addEventListener( 'load', function () {
 			switch ( extension ) {
 
 				case 'glb':
+				case 'gltf':
 
 					viewer.src = URL.createObjectURL( file );
 
@@ -76,6 +85,8 @@ window.addEventListener( 'load', function () {
 					reader.addEventListener( 'load', function ( event ) {
 
 						var geometry = new PLYLoader().parse( event.target.result );
+
+						geometry.computeVertexNormals();
 
 						setCustomObject( new Mesh( geometry, DEFAULT_MATERAL ) );
 
